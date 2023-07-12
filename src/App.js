b056d820cc6f8455e7a6613336aa3424385d9c3e
Ronaldo7Messi10 <51509375+Ronaldo7Messi10@users.noreply.github.com>
2023-07-12 17:09:22 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div className="intro-section">
+        Vignesh Kalavakuri
+        <div> Web Developer </div>
+      </div>
+
+      <div className="projects-section">
+        Vignesh Kalavakuri
+        <div> Web Developer </div>
+      </div>
+
+      <div className="skills-section">
+        Vignesh Kalavakuri
+        <div> Web Developer </div>
+      </div>
+
+      <div className="education-section">
+        Vignesh Kalavakuri
+        <div> Web Developer </div>
+      </div>
+      <div className="contact-section">
+        Conatact : vignesh@creative.com
+        <div className="contact-profiles">
+          <div> GitHub </div>
+          <div> Linkedin </div>
+          <div> Youtube </div>
+        </div>
+      </div>
+    </>
   );
 }
